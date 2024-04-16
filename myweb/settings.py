74265f480532fa4f89cc4dbd4670f3ab129f98db
Lily Jeong <lily.jeong0628@gmail.com>
2024-04-16 20:13:30 +0900

    chapter 2 in progres

--- myweb/settings.py
+++ myweb/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'photo',
 ]
+
+TIME_ZONE = 'Asia/Seoul'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
